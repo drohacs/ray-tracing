84853b786be64b069542d931c9f4ac4ef7e80366
Daniel <drohacs1@gmail.com>
2023-01-02 10:11:29 -0500

    updated scene

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "utilities.hpp";
+#include "utilities.hpp"
 
 #include "hittable_list.hpp"
 #include "color.hpp"
@@ -29,13 +29,13 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
     if (depth <= 0)
-        return color(0,0,0);    
+        return color(0,0,0);
+
     if (world.hit(r, 0.001, infinity, rec)) {
         ray scattered;
         color attenuation;
-        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
             return attenuation * ray_color(scattered, world, depth-1);
-        }
         return color(0,0,0);
     }
 
@@ -57,8 +57,8 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
